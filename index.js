32798b9ff217cc53a2c3f6d699bf198c0714cec2
aminariaz1 <riazamina990@gmail.com>
2024-11-28 23:36:49 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/*export const hello=(a,b,c)=>{
+return a*b*c;
+}*/
+
+
+export const hello=(a,b,c)=>{
+    return a*b*c;
+}
---
+++ script.js
@@ -0,0 +1,33 @@
+/*import { hello } from "./index.js"
+let ok = hello(12, 12, 3)
+console.log(ok);*/
+
+import { hello } from "./index.js";
+let ok = hello (12,2,4)
+console.log(ok)
+ 
+
+
+
+
+
+/*const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+g =JSON.stringify(person);
+console.log(string);
+
+let ok1 =JSON.parse(string)
+console.log(ok1);*/
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+}
+let ok2 =JSON.stringify(person)
+console.log(ok1)
+
+let ok1 =JSON.parse(String)
+console.log(ok1)
